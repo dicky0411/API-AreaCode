0d05f8542053b8cb1e12c5a6d019eaabdd7e257c
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-24 16:52:20 +0800

    Update AreaCodeController.php

--- api-areacode/app/Http/Controllers/AreaCodeController.php
+++ api-areacode/app/Http/Controllers/AreaCodeController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use GB2260;
 
-
 class AreaCodeController extends Controller
 {
     public function getAreaCode($ac = null)
     {
         $gb2260 = new \GB2260\GB2260();
         $result = $gb2260->get($ac);
-        
 
-        // Optionally, you can return the result as a JSON response
-        return response()->json($result);
+        if ($result === null) {
+            return response()->json(['error' => 'Area code not found'], 404, [], JSON_PRETTY_PRINT);
+        }
+
+        return response()->json($result, 200, [], JSON_PRETTY_PRINT);
     }
 }
